33647a93cf2f704f81f46cedb355e6971efeaa70
Akash-H-M <akashhm2000@gmail.com>
2021-10-06 12:55:11 +0530

    this  is sample git project

---
+++ sample/src/sample/First.java
@@ -0,0 +1,20 @@
+package sample;
+
+
+public class First {
+	public static void main(String[] args) {
+		
+		
+		int i=0;
+		while(i<10)
+		{
+			if(i%2==0)
+			{
+				System.out.println("hii akash!");
+			}
+			else
+				System.out.println("welcome to git...");
+			i++;
+		}
+	}
+}
